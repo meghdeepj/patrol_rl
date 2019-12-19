8b0b92e688d2744e1c173e63a2b077565b1a77b8
meghdeepj <meghdeepj@gmail.com>
2019-12-19 15:27:24 +0530

    ucb-mabp

--- prac_code/mab_ucb.py
+++ prac_code/mab_ucb.py
@@ -1,8 +1,8 @@
 #MABP python implement /w OpenAI Gym
-#epsilon-t-greedy algorithm
+#epsilon-greedy algorithm
 import gym
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 from gym import envs
 import numpy as np
 import gym_bandits
@@ -11,9 +11,6 @@ import gym_bandits
 env = gym.make('BanditTenArmedGaussian-v0')
 
 #run this to rand_init the env
-np.random.seed(42)
-env.seed(34)
-
 #number of actions
 n_ban= 10
 
@@ -23,58 +20,53 @@ q_table=np.zeros(n_ban)
 #tables to keep track of no. of times arm is pulled
 n_table=np.zeros(n_ban)
 
+#initialise ucb
+ucb=np.zeros(n_ban)
+
 #get initial observation
 env.reset()
-count = 1.0
 
+rew=[]
+ep=[]
 #get type and no. of actions
 print(env.action_space)
 
-#visualise convergence
-rew=[]
-ep =[]
-
 #iterate over episodes/rounds
-for episode_i in range(100):
+for episode_i in range(1000):
 	print("\n\nEpisode number: ", episode_i)
 	
-	r = np.random.random(1)[0]
-	print("random number: ", r)
-	
-	#decrese epsilon
-	epsilon = 1/count
-
-	print('Epsilon: ', epsilon)
-	#make the agent
-	if r >= epsilon:
-		best_idx= np.argmax(q_table)
-		action=best_idx
+	#ucb init
+	if episode_i < 10:
+		action = episode_i
 	else:
-		action=env.action_space.sample()
+		for i in range(n_ban):
+			up_bound = np.sqrt((2*np.log(sum(n_table)))/n_table[i])
+			ucb[i] = q_table[i]+up_bound
+		action = np.argmax(ucb)
 
+	print(pd.DataFrame({'UCB':ucb}))
 	print("action taken:", action)
 
 	#get return values on 4 variables after performing action
 	observation, reward, done, info = env.step(action)
-	rew.append(np.max(q_table))
-	ep.append(episode_i)
+
 	#UPDATE
 	n_table[action]+=1
 	q_table[action]=q_table[action]+ (1/n_table[action])*(reward-q_table[action])
-	
-	count+=0.2
-		
+	rew.append(np.max(q_table))
+	ep.append(episode_i)
 	#See results
 	print("Observation: ", observation)
 	print("Reward: ", reward)
 	show = pd.DataFrame({'Q-val':q_table, 'n_pulls': n_table})
 	show.index.name='arm'
 	print(show.head(10))
-	
+
 print("Most rewarding bandit is: ", np.argmax(q_table))
 print("It gave a reward of: ", round(np.max(q_table),3))
 plt.plot(ep,rew)
 plt.show()
+
 #close environment
 env.close()
 
