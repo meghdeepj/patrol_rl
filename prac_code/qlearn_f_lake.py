04572faeaf771a3b169101396624d31e7210ba54
meghdeepj <meghdeepj@gmail.com>
2020-01-03 11:12:50 +0530

    fl8x8test

--- prac_code/qlearn_f_lake.py
+++ prac_code/qlearn_f_lake.py
@@ -1,5 +1,5 @@
-#Q-Learning python implement /w OpenAI Gym
-#Q-Learning algorithm on random walk
+#TD(0) python implement /w OpenAI Gym
+#Q-Learning algorithm on 8x8 FrozenLake
 import gym
 import pandas as pd
 import time
@@ -9,19 +9,15 @@ from gym import envs
 import numpy as np
 import seaborn as sns
 sns.set()
-from IPython.core.debugger import set_trace
 
-#from src.rlenvs.rlenvs.envs.gridworld import GridworldEnv
-env=gym.make('FrozenLake-v0', is_slippery=False)
-
-#env=GridworldEnv()
+env=gym.make('FrozenLake8x8-v0', is_slippery=False)
 
 print('State space: ', env.nS, "\nAction space: ", env.nA)
 
 gamma=0.95
-alpha=0.4
+alpha=0.3
 count=1.0
-n_ep=5000
+n_ep=10000
 r_tot=0.
 q = np.zeros([env.nS, env.nA])
 
@@ -40,6 +36,22 @@ def ept_greed(state,epsilon):
 			return np.argmax(q[state])
 	else:
 		return env.action_space.sample()
+
+def test_agent(env,q):
+	prev_state=env.reset()
+	print('Intelligent Agent Test')
+	time.sleep(1.0)
+	env.render()
+	while True:
+		action= np.argmax(q[prev_state])
+		next_state, reward, done, info = env.step(action)
+		next_action=np.argmax(q[next_state])
+		prev_state=next_state
+		action=next_action
+		env.render()
+		time.sleep(0.5)
+		if done:
+			break
 c_re=[]
 ep=[]
 for i in range(n_ep):
@@ -63,11 +75,15 @@ for i in range(n_ep):
 	r_tot+=r
 	ep.append(i)
 	c_re.append(r_tot/(i+1))
+
+test_agent(env,q)
+time.sleep(0.5)
+
 print('total reward: ', r_tot)
 print('avg reward :', r_tot/n_ep)
 print('final Q: ', q)
 plt.plot(ep,c_re)
-plt.title("Q-Learning on FrozenLake-v0")
+plt.title("Q-Learning on FrozenLake8x8-v0")
 plt.ylim(0,1)
 plt.xlabel("No. of iterations")
 plt.ylabel("Cumulative Average Reward")
